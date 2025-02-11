7b22a14be2c6498f4826cb46c6f2b6024889bf94
harishbhaik <kmohamedharish.bca@gmail.com>
2025-02-11 09:00:14 +0530

    new app route for

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return "Hello, AWS! welcome git  demo llama adam  "
 
+
+@app.route("/view",methods=['GET'])
+def view():
+    return "Hey this just test version to find the process"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
